0dcabd4c8bfa86e0c1261568467782a6835f70ea
root <root@Gustavs-Brbara>
2024-03-04 00:53:44 +0100

    nu finns lite om

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,47 @@
+
+<div class="container bg-white h-5/6 rounded-lg">
+
+  <h1 class="p-3 font-bold text-2xl">Vår historia</h1>
+  <p class="p-3 text-lg">
+
+    Det känns som igår när vi för första gången samlades som ett band, med drömmar och förhoppningar 
+    om att göra vår musik och dela den med världen. Nu, efter flera år av hängivenhet och passion, 
+    kan vi med stolthet säga att vi har gått långt sedan starten hösten 2015. Under denna tid har 
+    vi inte bara växt som individer utan även som musiker och som ett band. Vi har utforskat olika 
+    ljud, melodier och texter för att hitta vår unika röst och stil. 
+    Det har varit en resa av utforskning, utmaningar och triumfer.
+    <br><br>
+    Under de senaste åren har vi släppt tre album som har tagit oss och våra lyssnare på en 
+    resa genom olika känslor, berättelser och soundscapes. Varje album har varit en ny chans 
+    för oss att uttrycka oss och dela våra tankar och känslor med världen. Nu står vi inför 
+    ännu en spännande tid då vi är redo att släppa vårt fjärde album. Det är ett album som 
+    representerar vårt fortsatta arbete och vår utveckling som musiker. Vi är fyllda av 
+    förväntan och hoppas innerligt att ni, kära lyssnare och supportrar, kommer att vara 
+    med oss på denna nya del av vår resa.
+    <br><br>
+    Vi, Thatchers, är redo att ta scenen med storm och sprida vår musik till nya platser 
+    och publik runt om i världen. Vi älskar att få möjligheten att spela live och dela vår 
+    passion med er ansikte mot ansikte. Oavsett om det är på små intima spelningar eller på 
+    stora festivaler, är varje tillfälle att spela live en möjlighet för oss att skapa en minnesvärd 
+    upplevelse för både oss själva och er som lyssnar. Vi tror på kraften i musikens gemenskap 
+    och vi är tacksamma för varje tillfälle att skapa den magiska atmosfären tillsammans med er.
+    <br><br>
+    Så om ni vill att vi ska spela hos er, tveka inte att kontakta oss. Vi finns bara ett mail, 
+    ett samtal eller ett meddelande bort. Ni kan nå oss via vår mail, telefon, Facebook och Instagram. 
+    Vi ser fram emot att höra från er och förhoppningsvis få chansen att dela vår svängiga, jammiga och 
+    roliga musik med er live och direkt. Tack för att ni har följt med oss på denna resa, och vi ser 
+    fram emot alla kommande äventyr tillsammans med er!
+
+
+  </p>
+
+
+
+
+
+
+
+
+
+
+</div>
